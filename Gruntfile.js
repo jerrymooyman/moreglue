9c5ac6f4815614a04eb70627b01a63d005773483
Jerry Mooyman <jerr73au@gmail.com>
2015-06-10 15:37:11 +1000

    added karma and grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,19 @@ module.exports = function(grunt){
             dev: {
                 src: ['public/javascripts/*.js', 'app.js']
             }
+        },
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            }
         }
     }); 
 
     // load packages
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
 
     // setup tasks
     grunt.registerTask('default', ['jshint']);
